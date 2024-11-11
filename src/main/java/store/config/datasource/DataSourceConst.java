196b3a3e5da41e6659c50be3ba7d86d8a9512167
koosco <rnxogud123@naver.com>
2024-11-11 20:28:06 +0900

    fix: 파일 읽기 경로 수정
    앞에 `/`가 들어가 절대 경로로 불러오는 문제가 있었습니다.
    `/`를 지워 상대 경로를 이용해 파일을 읽도록 수정했습니다.

--- src/main/java/store/config/datasource/DataSourceConst.java
+++ src/main/java/store/config/datasource/DataSourceConst.java
@@ -4,7 +4,7 @@ public enum DataSourceConst {
 
     FILE_DELIMITER(","),
     PRODUCT_FILE_PATH("src/main/resources/products.md"),
-    PROMOTION_FILE_PATH("/src/main/resources/promotions.md");
+    PROMOTION_FILE_PATH("src/main/resources/promotions.md");
 
     private final String name;
 
