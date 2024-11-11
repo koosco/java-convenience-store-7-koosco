fc5dd5b9b5b8c7c9453e4966ad06c493ce3e96f0
koosco <rnxogud123@naver.com>
2024-11-11 18:30:24 +0900

    feat: 영수증 프로모션 적용 비용 계산 기능 추가
    프로모션 적용을 받은 비용을 출력합니다.

--- src/main/java/store/stock/domain/Order.java
+++ src/main/java/store/stock/domain/Order.java
@@ -22,4 +22,8 @@ public class Order {
     public List<FreeProductDto> getFreeProductDtos() {
         return freeProductDtos;
     }
+
+    public int getMembershipDiscountPrice() {
+        return membershipDiscountPrice;
+    }
 }
