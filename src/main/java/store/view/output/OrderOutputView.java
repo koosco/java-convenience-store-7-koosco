e7b745305e893641ebaae7f034eadc103258317e
koosco <rnxogud123@naver.com>
2024-11-11 21:43:31 +0900

    fix: 영수증 가격 출력 수정
    영수증 가격 출력시 모든 물품 합을 출력하도록 수정했습니다.

--- src/main/java/store/view/output/OrderOutputView.java
+++ src/main/java/store/view/output/OrderOutputView.java
@@ -15,7 +15,7 @@ public class OrderOutputView {
     private static void printProducts(Order order) {
         System.out.printf("%-13s%-8s%-9s\n", "상품명", "수량", "금액");
         order.getProductDtos()
-            .forEach(dto -> System.out.printf("%-14s%-9d%,-9d\n", dto.name(), dto.amount(), dto.price()));
+            .forEach(dto -> System.out.printf("%-14s%-9d%,-9d\n", dto.name(), dto.amount(), dto.amount() *dto.price()));
     }
 
     private static void printPromotionProducts(Order order) {
